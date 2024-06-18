c63496c58354b22545550b0f2f2e833dfcf2f7ed
al3sha9 <optimist.alishan@gmail.com>
2024-06-18 07:11:21 +0500

    added arrary sum question

---
+++ arrays_manip/arr_sum.js
@@ -0,0 +1,13 @@
+let arr = [2, 4, 6, 8, 10];
+
+function calArr(arr) {
+    let sum = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+console.log(calArr(arr))
+
