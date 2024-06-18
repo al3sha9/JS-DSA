b63448bb56020f3510807a2955bd364e4d0daf7e
al3sha9 <optimist.alishan@gmail.com>
2024-06-19 04:12:46 +0500

    big o starting

---
+++ arrays_manip/arr1.js
@@ -0,0 +1,8 @@
+let data = {
+    fullName: "ali",
+    myAge: 20,
+    whichPhone: "OP9PRO"
+}
+
+console.log(`Keys of the object are: ${Object.keys(data)}`);
+console.log(`Values in the object are: ${Object.values(data)}`);
